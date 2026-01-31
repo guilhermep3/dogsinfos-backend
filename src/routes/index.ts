fe0eb401d0a86efaeafa561d92f4a42a7ffa86b8
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2026-01-31 09:37:10 -0300

    fix: remote the test middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import dogsRoutes from "./dogsRoutes"
-import { middleware } from "./middleware";
 
 const router = express.Router();
 
-router.use(middleware);
 router.use('/dogs', dogsRoutes);
 
 export default router;
